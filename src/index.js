1873967f1fe35ed0b928eb208560687635d9be3b
antigosu <ant1gosu@yandex.ru>
2018-01-08 23:22:39 +0300

    Added and deployed gh-pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css";
@@ -14,11 +14,11 @@ import reducer from "./reducers";
 const store = createStore(reducer, devToolsEnhancer());
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
 registerServiceWorker();
